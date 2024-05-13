57aa403f13025e69cbc5f5b531fdcbf74694bdb5
elias provincia <provinciaelias@gmail.com>
2024-05-13 07:51:29 -0500

    fist commit

---
+++ src/main/java/com/batch/springBatch/SpringBatchAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.batch.springBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch/springBatch/SpringBatchAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.springBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
