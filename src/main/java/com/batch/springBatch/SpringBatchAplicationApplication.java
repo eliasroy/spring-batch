737e7a0ddb2e68dcec9b5f0e998465c5a51aa7e4
elias provincia <provinciaelias@gmail.com>
2024-05-13 14:59:25 -0500

    feat: config to run project

--- src/main/java/com/batch/springBatch/SpringBatchAplicationApplication.java
+++ src/main/java/com/batch/springBatch/SpringBatchAplicationApplication.java
@@ -2,8 +2,9 @@ package com.batch.springBatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringBatchAplicationApplication {
 
 	public static void main(String[] args) {
