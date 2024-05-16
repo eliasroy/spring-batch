53770bb4a0a4b44280e0b7cfbb4bc7372713daad
elias provincia <provinciaelias@gmail.com>
2024-05-16 08:37:40 -0500

    feat: add step writer

--- src/main/java/com/batch/springBatch/config/BatchConfiguration.java
+++ src/main/java/com/batch/springBatch/config/BatchConfiguration.java
@@ -1,6 +1,8 @@
 package com.batch.springBatch.config;
 
+import com.batch.springBatch.steps.ItemProcessorStep;
 import com.batch.springBatch.steps.ItemReaderStep;
+import com.batch.springBatch.steps.ItemWriterStep;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,13 @@ public class BatchConfiguration {
     @Bean
     public ItemReaderStep itemReaderStep(){
         return new ItemReaderStep();
+    }
+    @Bean
+    public ItemProcessorStep itemProcessorStep(){
+        return new ItemProcessorStep();
+    }
+    @Bean
+    public ItemWriterStep itemWriterStep(){
+        return new ItemWriterStep();
     }
 }
--- src/main/java/com/batch/springBatch/steps/ItemWriterStep.java
+++ src/main/java/com/batch/springBatch/steps/ItemWriterStep.java
@@ -1,13 +1,42 @@
 package com.batch.springBatch.steps;
 
+import java.util.List;
+
+import com.batch.springBatch.entities.Person;
+import com.batch.springBatch.service.IPersonService;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
 
 public class ItemWriterStep implements Tasklet {
+    @Autowired
+    private  IPersonService personService;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        return null;
+        log.info("inicio de step writer");
+        log.info("------------------");
+        log.info("obtiene la lista de personas");
+        List<Person> personList=(List<Person>) chunkContext.getStepContext()
+                .getStepExecution()
+                .getJobExecution()
+                .getExecutionContext()
+                .get("personsList");
+        log.info("imprime si no es null");
+        personList.forEach(person -> {
+            if (person!=null){
+                log.info(person.toString());
+            }
+        });
+        log.info("guarda en la base de datos");
+        personService.saveAll(personList);
+        log.info("fin de step writer");
+        return RepeatStatus.FINISHED;
     }
 }
